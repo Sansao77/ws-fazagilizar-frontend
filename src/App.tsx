3579b77ce6d7a126476a481b9808ef879f8e1cbf
Sansao77 <gabrielp_santos@hotmail.com>
2025-03-07 14:14:32 -0300

    feat: sidebar first version completed

--- src/App.tsx
+++ src/App.tsx
@@ -44,9 +44,6 @@ import '@ionic/react/css/display.css';
 /* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
-/* Theme variables */
-import './theme/variables.css';
-
 setupIonicReact();
 
 const App: React.FC = () => (
--- src/templates/sidebar/Sidebar.tsx
+++ src/templates/sidebar/Sidebar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.scss';
 import { allMenu } from './sidebar-content';
 import { IonContent, IonHeader, IonIcon, IonImg, IonMenu } from '@ionic/react';
 import { chevronUp,chevronDown } from 'ionicons/icons';
+import { IMenuItem } from '../../interfaces/sidebar';
 
 const Sidebar: React.FC = () => {
+    const [sideContent, setSideContent] = useState(allMenu);
+
+    function setVisibility(title: string){
+        const updateSideBar = (sideContent:IMenuItem[], title: string):IMenuItem[] => 
+            sideContent.map((content) => 
+                content.title === title?
+                { ...content, showChildren: !content.showChildren }
+                : content.children
+                ? { ...content, children: updateSideBar(content.children, title)}
+                : content
+            )
+
+        setSideContent((content) => updateSideBar(content, title));
+    }
+
     return (
         <IonMenu side='start' contentId='app' menuId='side-menu'>
             <IonHeader>
@@ -26,25 +42,99 @@ const Sidebar: React.FC = () => {
             </IonHeader>
 
             <IonContent>
-                <div>
-                    {allMenu.map((x)=>
-                        <a href={x.route} className='align-component'>
+                <div className='padding-inside'>
+                    {sideContent.map((x)=>
+                        (x.children === undefined)? 
+                        (
+                        <a href={x.route} key={x.title} className='align-component first-component'>
                             <div className='align-link'>
-                                <IonIcon 
-                                slot='icon-only'
-                                icon={x.icon}
-                                />
+                                {x.icon &&
+                                    <IonIcon 
+                                    slot='icon-only'
+                                    icon={x.icon}
+                                    />
+                                }
 
                                 <span>{x.title}</span>
                             </div>
-
-                            {x.children && 
-                            <IonIcon
-                            slot='icon-only'
-                            icon={x.showChildren ? chevronUp: chevronDown}
-                            />
-                            }
                         </a>
+                        ):
+                        (
+                        <div key={x.title}>
+                            <span className='align-component first-component' onClick={() => setVisibility(x.title)}>
+                                <div className='align-link'>
+                                    {x.icon &&
+                                        <IonIcon 
+                                        slot='icon-only'
+                                        icon={x.icon}
+                                        />
+                                    }
+
+                                    <span>{x.title}</span>
+                                </div>
+
+                                <IonIcon
+                                slot='icon-only'
+                                icon={x.showChildren ? chevronUp: chevronDown} />
+                            </span>
+
+                            {(x.showChildren === true) && 
+                                x.children.map(y => 
+                                    (y.children === undefined)?
+                                    (
+                                        <a href={y.route} key={y.title}  className='align-component'>
+                                            <div className='align-link'>
+                                                {y.icon &&
+                                                    <IonIcon 
+                                                    slot='icon-only'
+                                                    icon={y.icon}
+                                                    className='border-inside'
+                                                    />
+                                                }
+
+                                                <span className={y.icon? '' : 'border-inside'}>{y.title}</span>
+                                            </div>
+                                        </a>
+                                    ):
+                                    (
+                                        <div key={y.title}>
+                                            <span className='align-component'>
+                                                <div className='align-link'>
+                                                    {y.icon &&
+                                                        <IonIcon 
+                                                        slot='icon-only'
+                                                        icon={y.icon}
+                                                        className='border-inside'
+                                                        />
+                                                    }
+
+                                                    <span className={y.icon? '' : 'border-inside'}>{y.title}</span>
+                                                </div>
+                                            </span>
+
+                                            <div className='padding-inside'>
+                                                {y.children.map(z =>
+                                                    <a href={z.route} key={z.title} className='align-component'>
+                                                        <div className='align-link gray-component'>
+                                                            {z.icon &&
+                                                                <IonIcon 
+                                                                slot='icon-only'
+                                                                icon={z.icon}
+                                                                className='border-inside'
+                                                                />
+                                                            }
+
+                                                            <span className={z.icon? '' : 'border-inside'}>{z.title}</span>
+                                                        </div>
+                                                    </a>
+                                                )}
+                                            </div>
+                                        </div>
+                                    )
+                                )
+                            }
+                        </div>
+                        )
                     )}
                 </div>
             </IonContent>
