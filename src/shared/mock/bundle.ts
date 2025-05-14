5e18a53cbfb7cc48cd94713164c2a245a68d1255
Sansao77 <gabrielp_santos@hotmail.com>
2025-05-14 17:05:36 -0300

    refactor: alteração das imagens mock no bundle

---
+++ src/shared/mock/bundle.ts
@@ -0,0 +1,26 @@
+export const bundleData = [
+    {
+        title: 'marca',
+        img: '/logos-dash/bundle/marca.jpeg'
+    },
+    {
+        title: 'promocao',
+        img: '/logos-dash/bundle/promocao.jpeg'
+    },
+    {
+        title: 'wa_representacoes',
+        img: '/logos-dash/bundle/wa_representacoes.jpeg'
+    },
+    {
+        title: 'agilichat',
+        img: '/logos-dash/bundle/agilichat.jpeg'
+    },
+    {
+        title: 'erp',
+        img: '/logos-dash/bundle/erp.jpeg'
+    },
+    {
+        title: 'graficos',
+        img: '/logos-dash/bundle/graficos.jpeg'
+    },
+]
