d5a9d7871b238a69a8c8f83df55e97927cf61c82
oyyago <yyagoaraujo@gmail.com>
2025-06-16 20:47:25 -0300

    feat: adicionar página de pedidos e componentes relacionados - Adicionada a rota para a nova página de pedidos no HomeRoutes - Criado componente Ordereds com formulário de filtro e botões de ação - Estilização do componente OrderFilterForm e criação de estilos para novos componentes - Renomeação da classe CSS 'dash-header' para 'dash-header-card' no componente Dash

--- src/modules/components/pages/home.routes.tsx
+++ src/modules/components/pages/home.routes.tsx
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 import Dash from "./dashboard/Dash";
 import ProductManagerRoutes from "./product-manager/product-manager.routes";
+import Ordereds from "./ordereds/ordereds";
 
 const HomeRoutes = () =>{
     return (
         <>
             <Route exact path="/home/dash" component={Dash}/>
+            <Route exact path="/home/ordereds" component={Ordereds}/>
             <ProductManagerRoutes />
         </>
     )
